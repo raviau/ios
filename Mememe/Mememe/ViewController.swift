0036611d3459780ef9a51e4de6781f70a68dded2
Ravi Maravar <rmaravar@gmail.com>
2020-03-04 19:18:27 +1100

    pick  image code

--- Mememe/Mememe/ViewController.swift
+++ Mememe/Mememe/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func pickImage(_ sender: Any) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate  = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[.originalImage] as? UIImage{
+            imageView.image = image
+        } else {
+            print("unable to load image")
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
